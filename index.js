0a686a072cfa4a7e72ecc47b10c8e8897a168186
Steve Streza <steve@stevestreza.com>
2021-09-04 16:36:02 -0700

    More discord

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-import Discord from "./lib/discord.js"
+import getDiscordClient from "./lib/discord.js"
 
-console.log(`Discord: ${Discord}`)
+const run = async () => {
+	const discord = await getDiscordClient()
+	const guilds = await discord.guilds.fetch()
+	guilds.each(guild => console.log(`GUILD '${guild.id}' named '${guild.name}':`, guild))
+	console.log(`Discord client is ready: `, guilds)
+}
+
+run().then(
+	() => {
+		process.exit(0)
+	},
+	err => {
+		console.error(`ERROR:`, err)
+		process.exit(1)
+	}
+)

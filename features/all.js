68949cec9b2b47a97761d2fbc7aef486bfb8d97e
SarahLynne <saralain88@gmail.com>
2023-09-21 19:32:39 -0400

    Kill badge notifier, moved to nocodb webhook

--- features/all.js
+++ features/all.js
@@ -1,8 +1,7 @@
 import NewBadges from "./new-badges/index.js"
 import ScheduleEvents from "./schedule-events/index.js"
-import BadgeNotifications from "./badge-notifications/index.js"
 
-const allFeatures = [NewBadges, BadgeNotifications, ScheduleEvents]
+const allFeatures = [NewBadges, ScheduleEvents]
 
 export const setupFeatures = async () => {
 	return Promise.all(allFeatures.map(feature => feature()))
